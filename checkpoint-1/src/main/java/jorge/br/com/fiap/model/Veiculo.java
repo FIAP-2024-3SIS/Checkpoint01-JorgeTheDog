dc0ccca9b4f11a84ee697feb34b2a049780496da
Erik Ko <97708892+erikhko@users.noreply.github.com>
2024-03-30 13:52:44 -0300

    Adicionando classes model

---
+++ checkpoint-1/src/main/java/jorge/br/com/fiap/model/Veiculo.java
@@ -0,0 +1,18 @@
+package jorge.br.com.fiap.model;
+
+public class Veiculo {
+	String tipoDoVeiculo;
+	String placa;
+	double velocidadeMedia;
+	
+	
+	public double getVelocidadeMedia() {
+		return velocidadeMedia;
+	}
+	
+	public void setVelocidadeMedia(double velocidadeMedia) {
+		this.velocidadeMedia = velocidadeMedia;
+	}
+	
+	
+}
---
+++ checkpoint-1/src/main/java/jorge/br/com/fiap/model/Viagem.java
@@ -0,0 +1,17 @@
+package jorge.br.com.fiap.model;
+
+public class Viagem {
+	String origem;
+	String destino;
+	double distancia;
+	
+	public double getDistancia() {
+		return distancia;
+	}
+	
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	
+	
+}
